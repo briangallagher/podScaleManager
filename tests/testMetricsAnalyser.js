71f9f9ac8bac50fa3cf2b278d498ad9b038007c3
Brian Gallagher <brian.gallagher@feedhenry.com>
2017-03-15 16:33:21 +0000

    updated metrics analysis

--- tests/testMetricsAnalyser.js
+++ tests/testMetricsAnalyser.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var proxyquire = require('proxyquire');
-// var metricsAnalyser = require('../lib/metricsAnalyser');
 var assert = require('assert');
 
 describe('Test metricsAnalyser', function () {
@@ -15,10 +14,15 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
       }
     });
 
-    var data = [451, 0, 1];
+    var data = [451, 0, 1, 1];
     metricsAnalyser.go(data);
 
     setTimeout(function () {
@@ -36,19 +40,50 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
       }
     });
 
-    var data = [449, 0, 1];
+    var data = [451, 0, 2, 1];
     metricsAnalyser.go(data);
 
     setTimeout(function () {
-      assert.equal(count, 3, 'Count is wrong');
+      assert.equal(count, 4, 'Count is wrong');
       done();
     }, 500);
   });
 
-  it('should successfully scale down based on cpu load ', function (done) {
+  it('should not try to scale as we have sufficient pods', function (done) {
+
+    var count = -1;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
+      }
+    });
+
+    var data = [451, 0, 4, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, -1, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down by 1, form 5 to 4', function (done) {
 
     var count = 0;
 
@@ -57,35 +92,71 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
       }
     });
 
-    var data = [449, 0, 10];
+    var data = [451, 0, 5, 1];
     metricsAnalyser.go(data);
 
     setTimeout(function () {
-      assert.equal(count, 3, 'Count is wrong');
+      assert.equal(count, 4, 'Count is wrong');
       done();
     }, 500);
   });
 
-  it('should not scale down past 1 pod', function (done) {
+  it('should scale down by 1, form 6 to 5', function (done) {
 
-    var count = 1;
+    var count = 0;
 
     var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
       './scalePods': {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
       }
     });
 
-    var data = [0, 0, 1];
+    var data = [451, 0, 6, 1];
     metricsAnalyser.go(data);
 
     setTimeout(function () {
-      assert.equal(count, 1, 'Count is wrong');
+      assert.equal(count, 5, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down by 1, from 10 to 9', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
+      }
+    });
+
+    var data = [451, 0, 10, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 9, 'Count is wrong');
       done();
     }, 500);
   });
@@ -99,11 +170,16 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, memoryOnlyModel);
+        }
       }
     });
 
-    var data = [0, 451, 1];
-    metricsAnalyser.go(data, true);
+    var data = [0, 301, 1, 1];
+    metricsAnalyser.go(data);
 
     setTimeout(function () {
       assert.equal(count, 4, 'Count is wrong');
@@ -120,19 +196,50 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, memoryOnlyModel);
+        }
       }
     });
 
-    var data = [0, 449, 1];
-    metricsAnalyser.go(data, true);
+    var data = [0, 301, 2, 1];
+    metricsAnalyser.go(data);
 
     setTimeout(function () {
-      assert.equal(count, 3, 'Count is wrong');
+      assert.equal(count, 4, 'Count is wrong');
       done();
     }, 500);
   });
 
-  it('should successfully scale down based on memory load ', function (done) {
+  it('should not try to scale as we have sufficient pods for required memory ', function (done) {
+
+    var count = -1;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, memoryOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 301, 4, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, -1, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down by 1, form 5 to 4 to meet memory needs', function (done) {
 
     var count = 0;
 
@@ -141,11 +248,94 @@ describe('Test metricsAnalyser', function () {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, memoryOnlyModel);
+        }
       }
     });
 
-    var data = [0, 449, 10];
-    metricsAnalyser.go(data, true);
+    var data = [0, 301, 5, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 4, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down by 1, form 6 to 5 to meet memory needs', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, memoryOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 301, 6, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 5, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down by 1, from 10 to 9  to meet memory needs', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 301, 10, 1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 9, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should successfully scale up based on io load', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, ioOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 0, 2, 2.1];
+    metricsAnalyser.go(data);
 
     setTimeout(function () {
       assert.equal(count, 3, 'Count is wrong');
@@ -153,20 +343,207 @@ describe('Test metricsAnalyser', function () {
     }, 500);
   });
 
-  it('should not scale down past 1 pod', function (done) {
+  it('should successfully scale up based on io load, test-2 ', function (done) {
 
-    var count = 1;
+    var count = 0;
 
     var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
       './scalePods': {
         scale: function (numPods) {
           count = numPods;
         }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, ioOnlyModel);
+        }
       }
     });
 
-    var data = [0, 0, 1];
-    metricsAnalyser.go(data, true);
+    var data = [0, 0, 2, 10];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 3, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should not try to scale as io demand is being met ', function (done) {
+
+    var count = -1;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, ioOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 0, 2, 1.5];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, -1, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down based on current io rate ', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, ioOnlyModel);
+        }
+      }
+    });
+
+    var data = [0, 0, 4, 0.5];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 3, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale up when cpu is high and memory is low ', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuAndMemoryModel);
+        }
+      }
+    });
+
+    var data = [451, 50, 2, 0];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 4, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale up when memory is high and cpu is low ', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuAndMemoryModel);
+        }
+      }
+    });
+
+    var data = [50, 301, 2, 0];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 4, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down when memory is low and cpu is low ', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuAndMemoryModel);
+        }
+      }
+    });
+
+    var data = [10, 10, 4, 0];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 3, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale up when cpu and memory are low and io is high', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuMemoryAndIoModel);
+        }
+      }
+    });
+
+    var data = [10, 10, 2, 10];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, 3, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
+  it('should scale down when cpu, memory and io are low', function (done) {
+
+    var count = 0;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuMemoryAndIoModel);
+        }
+      }
+    });
+
+    var data = [10, 10, 2, 0.5];
+    metricsAnalyser.go(data);
 
     setTimeout(function () {
       assert.equal(count, 1, 'Count is wrong');
@@ -174,4 +551,75 @@ describe('Test metricsAnalyser', function () {
     }, 500);
   });
 
+  it('should scale not scale up or down when all thresholds are within limits', function (done) {
+
+    var count = -1;
+
+    var metricsAnalyser = proxyquire('../lib/metricsAnalyser', {
+      './scalePods': {
+        scale: function (numPods) {
+          count = numPods;
+        }
+      },
+      './dataModel': {
+        getModel: function (cb) {
+          cb(null, cpuMemoryAndIoModel);
+        }
+      }
+    });
+
+    var data = [10, 10, 1, 1.1];
+    metricsAnalyser.go(data);
+
+    setTimeout(function () {
+      assert.equal(count, -1, 'Count is wrong');
+      done();
+    }, 500);
+  });
+
 });
+
+var cpuOnlyModel = {
+  "default": {
+    "cpuTresholdPercent": 50
+  }
+};
+
+var memoryOnlyModel = {
+  "default": {
+    "memoryTresholdPercent": 50
+  }
+};
+
+var ioOnlyModel = {
+  "default": {
+    "upperResponseTimeTresholdSeconds": 2,
+    "lowerResponseTimeTresholdSeconds": 1
+  }
+};
+
+var cpuAndMemoryModel = {
+  "default": {
+    "memoryTresholdPercent": 50,
+    "cpuTresholdPercent": 50
+  }
+};
+
+var cpuMemoryAndIoModel = {
+  "default": {
+    "memoryTresholdPercent": 50,
+    "cpuTresholdPercent": 50,
+    "upperResponseTimeTresholdSeconds": 2,
+    "lowerResponseTimeTresholdSeconds": 1
+  }
+};
+
+// var cpuOnlyModel = {
+//   "default": {
+//     "cpuTresholdPercent": 50, // percent of cpu limit applied
+//     "memoryTresholdPercent": 50, // percent of memoryTresholdPercent limit applied
+//     "upperResponseTimeTresholdSeconds": 2,
+//     "lowerResponseTimeTresholdSeconds": 1,
+//     "metricsAnalysisLevel": "high", // Should we spin down mulitple pods and how often should we check for changes
+//   }
+// }
