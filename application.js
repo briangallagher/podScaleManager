318eda27004ee7645cd978b5da2e22a9e1f110db
Brian Gallagher <brian.gallagher@feedhenry.com>
2017-03-16 08:57:18 +0000

    updateing logs

--- application.js
+++ application.js
@@ -3,8 +3,6 @@ var metricsCapture = require('./lib/metricsCapture');
 var express = require('express');
 var app = express();
 
-metricsCapture.startMonitoring();
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
